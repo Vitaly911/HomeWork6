3663b621bafafb95ab7ffe8ab71ec0cdc7a602eb
Vitaly <sinicin1313@gmail.com>
2023-01-26 22:55:46 +0300

    first commit

---
+++ Задача 41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Console.WriteLine("Введите числа через пробел");
+// int [] numbers = GetArrayFromString(Console.ReadLine());
+// int[] mass = new int [number];
+// int max = 0;
+// for(int i = 0; i < number; i++)
+// {
+//  if(mass[i] > 0) max++;
+// }
+// Console.WriteLine(max);
+Console.WriteLine("Введите количество чисел");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int [number];
+int max = 0;
+for(int i = 0; i < number; i++)
+{
+ mass[i] = new Random().Next(-100, 100);
+ Console.WriteLine($"{mass[i]} ");  
+}
+for(int i = 0; i < number; i++)
+{
+ if(mass[i] > 0) max++;
+}
+Console.WriteLine();
+Console.WriteLine(max);
